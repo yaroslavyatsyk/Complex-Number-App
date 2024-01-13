a7a03e4b84e47e8b08a0853907addcf15c5dd2b0
Yaroslav9728 <yaroslav9728@gmail.com>
2024-01-12 20:56:38 -0500

    Result Window Set Up

--- ResultPage.xaml.cs
+++ ResultPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,25 @@ namespace Complex_Number_App
         public ResultPage()
         {
             this.InitializeComponent();
+        }
+
+        public ResultPage(string op, Complex_Number a, Complex_Number b, Complex_Number result)
+        {
+            this.InitializeComponent();
+            this.Operation.Text = op;
+            this.Real1.Text = a.Real.ToString();
+            this.Imaginary1.Text = a.Imaginary.ToString();
+            
+            this.Real2.Text = b.Real.ToString();
+            this.Imaginary2.Text = b.Imaginary.ToString();
+
+            this.ResReal.Text = result.Real.ToString();
+            this.ResIm.Text = result.Imaginary.ToString();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+           CoreWindow.GetForCurrentThread().Close();
         }
     }
 }
